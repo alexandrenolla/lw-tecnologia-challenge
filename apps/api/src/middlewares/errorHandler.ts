e637ffb04ef70e20493eb20a993265489ed30bca
Alexandre Henrique, O Grande <alexandre.nolla@gmail.com>
2025-12-03 00:01:35 -0300

    add error handler

---
+++ apps/api/src/middlewares/errorHandler.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+
+export class AppError extends Error {
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    Object.setPrototypeOf(this, AppError.prototype);
+  }
+}
+
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  if (err instanceof AppError) {
+    res.status(err.statusCode).json({ error: err.message });
+    return;
+  }
+
+  console.error("Unexpected error:", err);
+  res.status(500).json({ error: "Internal server error" });
+}
+
+type AsyncRequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => Promise<void>;
+
+export function asyncHandler(fn: AsyncRequestHandler) {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+}
