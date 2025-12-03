9b323a87bf7e345a952065f163e0cd504cc7475b
Alexandre Henrique, O Grande <alexandre.nolla@gmail.com>
2025-12-03 18:46:03 -0300

    add routes

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
 
+import { accountController } from "../controllers/accountController";
 import { authController } from "../controllers/authController";
+import { requireAuth } from "../middlewares/auth";
 
 const router = Router();
 
 // Public routes
 router.post("/login", authController.login);
 
-// Protected routes (to be implemented)
-// router.get("/balance", requireAuth, accountController.getBalance);
-// router.post("/event", requireAuth, accountController.handleEvent);
-// router.post("/reset", requireAuth, accountController.reset);
+// Protected routes
+router.get("/balance", requireAuth, accountController.getBalance);
+router.post("/event", requireAuth, accountController.handleEvent);
+router.post("/reset", requireAuth, accountController.reset);
 
 export default router;
